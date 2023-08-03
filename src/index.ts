f6a49f6c7e989c5808334256a5f828ff1fc78ee1
3obby <rfusseryiii+gh@gmail.com>
2023-08-02 21:07:11 -0500

    trimmed

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,24 @@
-import { extendConfig, extendEnvironment } from "hardhat/config";
+import { extendEnvironment } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
-import path from "path";
+import { ethers } from "ethers";
 
-import { ExampleHardhatRuntimeEnvironmentField } from "./ExampleHardhatRuntimeEnvironmentField";
-// This import is needed to let the TypeScript compiler know that it should include your type
-// extensions in your npm package's types file.
+// We're extending the Hardhat Runtime Environment, adding our hashing functionality
 import "./type-extensions";
 
-extendConfig(
-  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    // We apply our default config here. Any other kind of config resolution
-    // or normalization should be placed here.
-    //
-    // `config` is the resolved config, which will be used during runtime and
-    // you should modify.
-    // `userConfig` is the config as provided by the user. You should not modify
-    // it.
-    //
-    // If you extended the `HardhatConfig` type, you need to make sure that
-    // executing this function ensures that the `config` object is in a valid
-    // state for its type, including its extensions. For example, you may
-    // need to apply a default value, like in this example.
-    const userPath = userConfig.paths?.newPath;
+async function getHash(input: string) {
+  const message = ethers.toUtf8Bytes(input);
+  const messageHash = ethers.keccak256(message);
 
-    let newPath: string;
-    if (userPath === undefined) {
-      newPath = path.join(config.paths.root, "newPath");
-    } else {
-      if (path.isAbsolute(userPath)) {
-        newPath = userPath;
-      } else {
-        // We resolve relative paths starting from the project's root.
-        // Please keep this convention to avoid confusion.
-        newPath = path.normalize(path.join(config.paths.root, userPath));
-      }
-    }
+  // Convert the message hash (hex string) to BigInt, subtract 1, and convert back to hex string
+  const hashMinusOneBigInt = ethers.toBigInt(messageHash) - 1n;
+  const hashMinusOneHex = ethers.toBeHex(hashMinusOneBigInt);
 
-    config.paths.newPath = newPath;
-  }
-);
+  return hashMinusOneHex;
+}
 
 extendEnvironment((hre) => {
   // We add a field to the Hardhat Runtime Environment here.
   // We use lazyObject to avoid initializing things until they are actually
   // needed.
-  hre.example = lazyObject(() => new ExampleHardhatRuntimeEnvironmentField());
+  hre.getHash = lazyObject(() => getHash);
 });
