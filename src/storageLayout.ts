54b1771e096c4b9b54c9427845fc1935c4d94148
mhxw <32643286+mhxw@users.noreply.github.com>
2023-08-26 15:16:36 +0800

    update

--- src/storageLayout.ts
+++ src/storageLayout.ts
@@ -16,23 +16,23 @@ export class StorageLayout {
 
   public async export() {
 
-    function jsonToMarkdown(jsonData: any[]): string {
-      if (!Array.isArray(jsonData) || jsonData.length === 0) {
-        return '';
-      }
-
-      const headers = Object.keys(jsonData[0]);
-      const separator = headers.map(() => '---');
-      const rows = jsonData.map(obj => headers.map(key => obj[key]));
-
-      const table = [
-        headers.join(' | '),
-        separator.join(' | '),
-        rows.map(row => row.join(' | ')).join('\n'),
-      ].join('\n');
-
-      return table;
-    }
+    // function jsonToMarkdown(jsonData: any[]): string {
+    //   if (!Array.isArray(jsonData) || jsonData.length === 0) {
+    //     return '';
+    //   }
+    //
+    //   const headers = Object.keys(jsonData[0]);
+    //   const separator = headers.map(() => '---');
+    //   const rows = jsonData.map(obj => headers.map(key => obj[key]));
+    //
+    //   const table = [
+    //     headers.join(' | '),
+    //     separator.join(' | '),
+    //     rows.map(row => row.join(' | ')).join('\n'),
+    //   ].join('\n');
+    //
+    //   return table;
+    // }
     
     const storageLayoutPath = this.env.config.paths.newStorageLayoutPath;
     const outputDirectory = path.resolve(storageLayoutPath);
@@ -98,10 +98,10 @@ export class StorageLayout {
           outputDirectory + "/storage.json",
           JSON.stringify(data)
         );
-
-        // 生成 Markdown 文件
-        const markdownData = jsonToMarkdown(JSON.parse(JSON.stringify(data)));
-        fs.writeFileSync(outputDirectory +"/output.md", markdownData, 'utf-8');
+        //
+        // // 生成 Markdown 文件
+        // const markdownData = jsonToMarkdown(JSON.parse(JSON.stringify(data)));
+        // fs.writeFileSync(outputDirectory +"/output.md", markdownData, 'utf-8');
       }
     }
     const prettifier = new Prettify(data.contracts);
